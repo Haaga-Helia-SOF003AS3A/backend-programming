26e5d03cf84007402db251440374dd0a69c238e5
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-02-07 10:19:49 +0200

    minor changes

--- StudentListSignup/src/main/java/fi/haagahelia/course/StudentListSignUpApplication.java
+++ StudentListSignup/src/main/java/fi/haagahelia/course/StudentListSignUpApplication.java
@@ -2,18 +2,17 @@ package fi.haagahelia.course;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import fi.haagahelia.course.domain.AppUser;
+import fi.haagahelia.course.domain.AppUserRepository;
 import fi.haagahelia.course.domain.Department;
 import fi.haagahelia.course.domain.DepartmentRepository;
 import fi.haagahelia.course.domain.Student;
 import fi.haagahelia.course.domain.StudentRepository;
-import fi.haagahelia.course.domain.AppUser;
-import fi.haagahelia.course.domain.AppUserRepository;
 
 @SpringBootApplication
 public class StudentListSignUpApplication {
