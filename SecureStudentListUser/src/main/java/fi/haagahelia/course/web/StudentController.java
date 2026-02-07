1640fb85ef74c81315542c55d71955e11cf2a83d
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-02-07 09:40:38 +0200

    updated class accroding spring boot 4 recommendations

--- SecureStudentListUser/src/main/java/fi/haagahelia/course/web/StudentController.java
+++ SecureStudentListUser/src/main/java/fi/haagahelia/course/web/StudentController.java
@@ -3,11 +3,12 @@ package fi.haagahelia.course.web;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,17 +19,17 @@ import fi.haagahelia.course.domain.StudentRepository;
 
 @Controller
 public class StudentController {
-	//@Autowired
-	private StudentRepository repository; 
+ 
+    // https://docs.spring.io/spring-boot/reference/using/spring-beans-and-dependency-injection.html
+ 	private final StudentRepository studentRepository; 
 
-	//@Autowired
-	private DepartmentRepository drepository; 
+	private final DepartmentRepository departmentRepository; 
 	
 	// Constructor Injection
 	public StudentController(StudentRepository studentRepository, 
 			DepartmentRepository departmentRepository) {
-		this.repository = studentRepository; 
-		this.drepository = departmentRepository;
+		this.studentRepository = studentRepository; 
+		this.departmentRepository = departmentRepository;
 	}
 	
 	// Show all students
@@ -41,34 +42,40 @@ public class StudentController {
     @RequestMapping(value="/studentlist")
     public String studentList(Model model) {
   	
-        model.addAttribute("students", repository.findAll());
+        model.addAttribute("students", studentRepository.findAll());
         return "studentlist";
     }
   
 	// RESTful service to get all students
     @RequestMapping(value="/students")
     public @ResponseBody List<Student> studentListRest() {	
-        return (List<Student>) repository.findAll();
+        return (List<Student>) studentRepository.findAll();
     }    
 
 	// RESTful service to get student by id
     @RequestMapping(value="/student/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Student> findStudentRest(@PathVariable("id") Long studentId) {	
-    	return repository.findById(studentId);
-    }       
+    	return studentRepository.findById(studentId);
+    }  
+    
+    // RESTful service to save a new student 
+    @PostMapping("/students")
+    public @ResponseBody Student saveStudent(@RequestBody Student student) {	
+    	return studentRepository.save(student);
+    } 
     
     // Add new student
     @RequestMapping(value = "/add")
     public String addStudent(Model model){
     	model.addAttribute("student", new Student());
-    	model.addAttribute("departments", drepository.findAll());
+    	model.addAttribute("departments", departmentRepository.findAll());
         return "addstudent";
     }     
     
     // Save new student
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Student student){
-        repository.save(student);
+        studentRepository.save(student);
         return "redirect:studentlist";
     }    
 
@@ -76,7 +83,7 @@ public class StudentController {
     @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteStudent(@PathVariable("id") Long studentId, Model model) {
-    	repository.deleteById(studentId);
+    	studentRepository.deleteById(studentId);
         return "redirect:../studentlist";
     }     
 }
