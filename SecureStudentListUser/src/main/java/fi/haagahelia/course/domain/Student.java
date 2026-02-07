10938996735ff3564a585e5b5165e3f9681b3747
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-02-07 09:41:17 +0200

    removed unused imports

--- SecureStudentListUser/src/main/java/fi/haagahelia/course/domain/Student.java
+++ SecureStudentListUser/src/main/java/fi/haagahelia/course/domain/Student.java
@@ -1,11 +1,13 @@
 package fi.haagahelia.course.domain;
 
-
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Student {
