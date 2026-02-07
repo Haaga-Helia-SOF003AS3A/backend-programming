737d52ab1b5256df7e037b81cc912e121f23580f
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-02-07 09:39:36 +0200

    updated security configurations so that also postman and h2-console are working

--- SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecureStudentListUser/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -1,14 +1,12 @@
 package fi.haagahelia.course;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,12 +15,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 	
 	//@Autowired
-	private UserDetailsService userDetailsService; // type of attribute -> interface
+	//private UserDetailsService userDetailsService; // type of attribute -> interface
 	
 	// Constructor injection
-	public WebSecurityConfig(UserDetailsService userDetailsService) {
-		this.userDetailsService = userDetailsService; 
-	}
+	//public WebSecurityConfig(UserDetailsService userDetailsService) {
+//		this.userDetailsService = userDetailsService; 
+//	}
+ 	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -31,8 +33,10 @@ public class WebSecurityConfig {
 				authorize -> authorize
 				.requestMatchers("/css/**").permitAll()
 				.requestMatchers("/api/students**").permitAll()
-				.requestMatchers(toH2Console()).permitAll()  // for h2console
+				.requestMatchers("/h2-console/**").permitAll() // for h2console
 				.anyRequest().authenticated())
+				// Käyttää HTTP Basic -autentikointia oletusasetuksilla (Postman)
+                .httpBasic(Customizer.withDefaults())
 				.headers(headers -> 
 					headers.frameOptions(frameOptions -> frameOptions 
 						.disable())) // for h2console
@@ -41,14 +45,8 @@ public class WebSecurityConfig {
 					.defaultSuccessUrl("/studentlist", true)
 					.permitAll())
 				.logout(logout -> logout.permitAll())
-				.csrf(csrf -> 
-					csrf.ignoringRequestMatchers(toH2Console())); // for h2console, not for production, just for development
-
+				.csrf(csrf -> csrf.disable()); // not for production, just for development
 		return http.build();
 	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}
 }
