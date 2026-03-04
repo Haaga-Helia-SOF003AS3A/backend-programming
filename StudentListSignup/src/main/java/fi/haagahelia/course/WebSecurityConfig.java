d6a3c8fb3aa353a4dada56f627d1c42fb78800c8
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-03-04 11:13:14 +0200

    inserted BCryptPasswordEncoder

--- StudentListSignup/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ StudentListSignup/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -1,12 +1,9 @@
 package fi.haagahelia.course;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,13 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig  {
 	
-	private final UserDetailsService userDetailsService; // interface type
-	
-	// Constructor injection
-	public WebSecurityConfig(UserDetailsService userDetailsService) {
-			this.userDetailsService = userDetailsService; 
-	}
-	
+	@Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 	// using lambda 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -44,8 +39,4 @@ public class WebSecurityConfig  {
 		return http.build();
 	}
 
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}}
+}
