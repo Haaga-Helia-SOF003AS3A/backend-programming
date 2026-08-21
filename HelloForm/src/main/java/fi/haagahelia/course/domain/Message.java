df2553f8eab4a5431e1935926f10a639e0c7f91b
Bergius Tanja <berta@haaga-helia.fi>
2026-08-21 10:11:52 +0300

    Upgraded to Spring Boot 4.1.0 etc

--- HelloForm/src/main/java/fi/haagahelia/course/domain/Message.java
+++ HelloForm/src/main/java/fi/haagahelia/course/domain/Message.java
@@ -2,21 +2,36 @@ package fi.haagahelia.course.domain;
 
 
 public class Message {
-	private Long id;
 
+	// attributes
+	private Long id;
 	private String msg;
-	
-	public Long getId() {
-		return id;
+
+	// constructors
+	public Message() {
+		this.id = null;
+		this.msg = null;
 	}
+	public Message(Long id, String msg) {
+		this.id = id;
+		this.msg = msg;
+	}
+	
+	// setters
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getMsg() {
-		return msg;
-	}
+
 	public void setMsg(String msg) {
 		this.msg = msg;
+	}
+
+	// getters
+	public Long getId() {
+		return id;
+	}
+	public String getMsg() {
+		return msg;
 	}
 	
 	@Override
--- HelloFormValidation/src/main/java/fi/haagahelia/course/domain/Message.java
+++ HelloFormValidation/src/main/java/fi/haagahelia/course/domain/Message.java
@@ -4,24 +4,41 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Message {
+	
+	// attributes
 	@NotNull
 	private Long id;
 	
     @Size(min=5, max=30)
 	private String msg;
 	
-	public Long getId() {
-		return id;
+	// constructors
+	public Message(Long id, String msg) {
+		this.id = id;
+		this.msg = msg;
 	}
+	public Message() {
+		this.id = null;
+		this.msg = null;
+	}
+
+	// setterit
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	// getterit
+	public Long getId() {
+		return id;
 	}
 	public String getMsg() {
 		return msg;
 	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
+	
 	
 	@Override
 	public String toString() {
