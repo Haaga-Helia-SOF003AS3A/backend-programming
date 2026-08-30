aa43b88eda1ead32fb37485779c4a41fa89fd611
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-08-30 16:47:39 +0300

    field injection to constructor injection & updgrade SB version to 4.1.1

--- StudentList/src/main/java/fi/haagahelia/course/domain/StudentRepository.java
+++ StudentList/src/main/java/fi/haagahelia/course/domain/StudentRepository.java
@@ -1,10 +1,7 @@
 package fi.haagahelia.course.domain;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
-    
 }
--- StudentList/src/main/java/fi/haagahelia/course/web/StudentController.java
+++ StudentList/src/main/java/fi/haagahelia/course/web/StudentController.java
@@ -1,6 +1,5 @@
 package fi.haagahelia.course.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +8,18 @@ import fi.haagahelia.course.domain.StudentRepository;
 
 @Controller
 public class StudentController {
-	@Autowired
-	private StudentRepository repository; 
-	
-    @RequestMapping(value= {"/", "/studentlist"})
-    public String studentList(Model model) {	
+
+    // https://docs.spring.io/spring-framework/reference/core/beans/dependencies/factory-collaborators.html
+    private final StudentRepository repository;
+
+    public StudentController(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = { "/", "/studentlist" })
+    public String studentList(Model model) {
         model.addAttribute("students", repository.findAll());
         return "studentlist";
     }
-  
+
 }
