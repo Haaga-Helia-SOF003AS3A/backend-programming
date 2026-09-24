9b8b2c54a9f1c8bc383f5dacc8a15d51ea680810
h01340@haaga-helia.fi <h01340@haaga-helia.fi>
2026-09-24 12:05:35 +0300

    sb version upgrade and testing clarifications

--- StudentJpaTest/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ StudentJpaTest/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
-	
 
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -26,7 +24,7 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-	@Bean BCryptPasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+	// @Bean BCryptPasswordEncoder passwordEncoder(){
+	// return new BCryptPasswordEncoder();
+	// }
 }
--- StudentJpaTest/src/main/java/fi/haagahelia/course/web/StudentRestController.java
+++ StudentJpaTest/src/main/java/fi/haagahelia/course/web/StudentRestController.java
@@ -3,35 +3,38 @@ package fi.haagahelia.course.web;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.course.domain.Student;
 import fi.haagahelia.course.domain.StudentRepository;
 
+@RequestMapping("/students")
 @CrossOrigin
-@Controller
+@RestController
 public class StudentRestController {
 
-    private StudentRepository studentRepository; 
+    // https://docs.spring.io/spring-boot/reference/using/spring-beans-and-dependency-injection.html
+    private final StudentRepository studentRepository;
 
-    public StudentRestController(StudentRepository studentRepository){
+    // Constructor Injection
+    public StudentRestController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+
     }
 
     // RESTful service to get all students
-    @RequestMapping(value = "/students")
-    public @ResponseBody List<Student> studentListRest() {
+    @RequestMapping
+    public List<Student> studentListRest() {
         return (List<Student>) studentRepository.findAll();
     }
 
     // RESTful service to get student by id
-    @RequestMapping(value = "/students/{id}", method = RequestMethod.GET)
-    public @ResponseBody Optional<Student> findStudentRest(@PathVariable("id") Long studentId) {
+    @GetMapping(value = "/{id}")
+    public Optional<Student> findStudentRest(@PathVariable("id") Long studentId) {
         return studentRepository.findById(studentId);
     }
 
